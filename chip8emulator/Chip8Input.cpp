01160a992f6ef41f72a3215a9d9cd89c3b3d8fa5
spacebruce <noel.abercrombie@hotmail.com>
2020-09-29 01:07:01 +0100

    Add typical "invaders" style input method

--- chip8emulator/Chip8Input.cpp
+++ chip8emulator/Chip8Input.cpp
@@ -20,6 +20,7 @@ void Chip8Input::Tick(Arduboy2& System)
   switch(this->InputMode)
   {
     case Chip8InputMode::Chord:
+    {
       uint8_t Offset = 0;
       if(System.pressed(A_BUTTON))
         Offset += 4;
@@ -33,6 +34,21 @@ void Chip8Input::Tick(Arduboy2& System)
         Emulator->SendInput(Offset + 2);
       if(System.pressed(RIGHT_BUTTON))
         Emulator->SendInput(Offset + 3);
-    return;
+      }
+    break;
+    case Chip8InputMode::Invaders:
+    {
+      if(System.pressed(A_BUTTON))
+        Emulator->SendInput(0x5);
+      if(System.pressed(UP_BUTTON))
+        Emulator->SendInput(0x2);
+      if(System.pressed(LEFT_BUTTON))
+        Emulator->SendInput(0x4);
+      if(System.pressed(RIGHT_BUTTON))
+        Emulator->SendInput(0x6);
+      if(System.pressed(DOWN_BUTTON))
+        Emulator->SendInput(0x8);
+    }
+    break;
   }
 }
--- chip8emulator/Chip8Input.h
+++ chip8emulator/Chip8Input.h
@@ -3,7 +3,7 @@
 
 enum class Chip8InputMode : uint8_t
 {
-  Chord,
+  Chord, Invaders,
 };
 
 /*  Modes
@@ -12,13 +12,16 @@ enum class Chip8InputMode : uint8_t
     A + U/R/D/L : 4,5,6,7
     B + U/R/D/L : 8,9,10,11
     A + B + U/R/D/L : 12,13,14,15
+  Invaders:
+    U/L/R/D : 2,4,6,8
+    A : 5
 */
 
 class Chip8Input
 {
 private:
   Chip8Emulator* Emulator;
-  Chip8InputMode InputMode = Chip8InputMode::Chord;
+  Chip8InputMode InputMode = Chip8InputMode::Invaders;
 public:
   Chip8Input(Chip8Emulator& Emulator);
   Chip8Input(Chip8Emulator& Emulator, Chip8InputMode InputMode);
