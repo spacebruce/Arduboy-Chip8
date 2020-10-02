30e5ae3e47d1f00d86180f56b8813b5d4cd72060
spacebruce <noel.abercrombie@hotmail.com>
2020-10-02 04:54:53 +0100

    Tweak character drawing
    getting closer

--- arduboychip8/src/System/Printer.h
+++ arduboychip8/src/System/Printer.h
@@ -47,7 +47,7 @@ public:
     uint8_t charBuffer[CharacterHeight];
     for(uint8_t i = 0; i < CharacterHeight; ++i)
     {
-      charBuffer[i] = pgm_read_byte(FontData[offset + i]);
+      charBuffer[i] = static_cast<uint8_t>(pgm_read_byte(&(FontData[offset + i])));
     }
 
     //Draw char to buffer
@@ -57,7 +57,7 @@ public:
       uint8_t charY = 0;
       for(uint8_t drawY = this->y; drawY < (this->y + this->CharacterHeight); ++drawY)
       {
-        const uint8_t colour = (charBuffer[charY] >> charX) & 0x1;
+        const uint8_t colour = (charBuffer[charY] >> (charX + 4)) & 0x1;
 
         const uint8_t columnHeight = 8;
         const size_t row = (drawY / columnHeight);
