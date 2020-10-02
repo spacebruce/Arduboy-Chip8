b4cac58e940cc58cab7eb137eceef1f21b2cdf5c
spacebruce <noel.abercrombie@hotmail.com>
2020-10-02 05:40:09 +0100

    Give the emulator it's own screen buffer

--- arduboychip8/src/States/EmulatorState.h
+++ arduboychip8/src/States/EmulatorState.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../System/Screen64x32.h"
 
 class Game;
 
@@ -7,7 +8,7 @@ class EmulatorState
 public:
 	void update(Game & game);
 	void render(Game & game);
-
+	Screen64x32 screen;
 private:
 	void updateInput(Game & game);
 };
