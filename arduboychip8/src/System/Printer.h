baadd2a1cbc2ae2f11dd29d7ec9de8e71983b930
spacebruce <noel.abercrombie@hotmail.com>
2020-10-02 05:33:08 +0100

    Handle control codes & non printables in Printer

--- arduboychip8/src/System/Printer.h
+++ arduboychip8/src/System/Printer.h
@@ -9,6 +9,20 @@ private:
   uint8_t x = 0, y = 0;
   const uint8_t CharacterWidth = 4;
   const uint8_t CharacterHeight = 5;
+
+  void newLine()
+  {
+    this->x = 0;
+    this->y += this->CharacterHeight + 1;
+  };
+  void nextLetter()
+  {
+    this->x += this->CharacterWidth + 1;
+    if((this->x + this->CharacterWidth) > this->bufferWidth)
+    {
+      this->newLine();
+    }
+  }
 public:
   Printer() = default;
   Printer(uint8_t* buffer, uint8_t width, uint8_t height)
@@ -28,22 +42,37 @@ public:
   }
   size_t write(uint8_t letter)
   {
-    //Find offset in font table
+    //Handle character
     uint8_t offset = 0x24;  //default to ?
-    if(letter >= 0x30 && letter <= 0x39)  //Numbers
-      offset = (letter - 0x30) + 0x0;
-    else if (letter >= 0x41 && letter <= 0x5A) //Capitals
-      offset = (letter - 0x41) + 0xA;
-    else if (letter >= 0x64 && letter <= 0x7A) //Lowercase
-      offset = (letter - 0x64) + 0xA;
-    else if(letter == 0x21)  // !
-      offset = 0x25;
-    else if(letter == 0x3F)  // ?
-      offset = 0x24;
+    switch(letter)
+    {
+      case '\n':
+        this->newLine();
+        return 1;
+      break;
+      case ' ':
+        this->nextLetter();
+        return 1;
+      break;
+      case 0x30 ... 0x39: //Numbers
+        offset = (letter - 0x30) + 0x0;
+      break;
+      case 0x41 ... 0x5A: //Uppercase
+        offset = (letter - 0x41) + 0xA;
+      break;
+      case 0x64 ... 0x7A: //Lowercase
+        offset = (letter - 0x64) + 0xA;
+      break;
+      case 0x21:  // !
+        offset = 0x25;
+      break;
+      default:  // ?
+        offset = 0x24;
+      break;
+    }
+    offset *= CharacterHeight;  //5 bytes per char (5 * 36 = 180, safely in 8bit range)
 
-    offset *= CharacterHeight;  //5 bytes per char
-
-    //Cache glyph
+    //Cache and preshift glyph
     uint8_t charBuffer[CharacterHeight];
     for(uint8_t i = 0; i < CharacterHeight; ++i)
     {
@@ -62,7 +91,7 @@ public:
         const uint8_t columnHeight = 8;
         const size_t row = (drawY / columnHeight);
         const size_t bitIndex = (drawY % columnHeight);
-        const size_t bufferIndex = ((row * bufferWidth) +drawX);
+        const size_t bufferIndex = ((row * bufferWidth) + drawX);
         const size_t bit = (1 << bitIndex);
 
         if(colour != 0)
@@ -76,12 +105,7 @@ public:
     }
 
     //Advance char position
-    this->x += this->CharacterWidth + 1;
-    if ((this->x + this->CharacterWidth) > this->bufferWidth)
-    {
-      this->x = 0;  //have a configurable x start position?
-      this->y += this->CharacterHeight + 1;
-    }
+    this->nextLetter();
 
     return 1; //what does this signal?
   }
