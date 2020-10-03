1a4113fdf46ebaf06f60d71a7287c7eb6f16d7f4
spacebruce <noel.abercrombie@hotmail.com>
2020-10-03 04:06:14 +0100

    Make characterWidth/Height static constexpr
    and normalize name style

--- arduboychip8/src/System/Printer.h
+++ arduboychip8/src/System/Printer.h
@@ -8,18 +8,18 @@ private:
   uint8_t bufferWidth, bufferHeight;
   uint8_t x = 0, y = 0;
   uint8_t xStart = 0;
-  const uint8_t CharacterWidth = 4;
-  const uint8_t CharacterHeight = 5;
+  static constexpr uint8_t characterWidth = 4;
+  static constexpr uint8_t characterHeight = 5;
 
   void newLine()
   {
     this->x = this->xStart;
-    this->y += this->CharacterHeight + 1;
+    this->y += this->characterHeight + 1;
   };
   void nextLetter()
   {
-    this->x += this->CharacterWidth + 1;
-    if((this->x + this->CharacterWidth) > this->bufferWidth)
+    this->x += this->characterWidth + 1;
+    if((this->x + this->characterWidth) > this->bufferWidth)
     {
       this->newLine();
     }
@@ -71,21 +71,21 @@ public:
         offset = 0x24;
       break;
     }
-    offset *= CharacterHeight;  //5 bytes per char (5 * 36 = 180, safely in 8bit range)
+    offset *= characterHeight;  //5 bytes per char (5 * 36 = 180, safely in 8bit range)
 
     //Cache and preshift glyph
-    uint8_t charBuffer[CharacterHeight];
-    for(uint8_t i = 0; i < CharacterHeight; ++i)
+    uint8_t charBuffer[characterHeight];
+    for(uint8_t i = 0; i < characterHeight; ++i)
     {
       charBuffer[i] = static_cast<uint8_t>(pgm_read_byte(&(FontData[offset + i])) >> 4) & 0x0F;
     }
 
     //Draw char to buffer
-    uint8_t charX = CharacterWidth - 1;
-    for(uint8_t drawX = this->x; drawX < (this->x + this->CharacterWidth); ++drawX)
+    uint8_t charX = characterWidth - 1; //draw byte backwards. Why is it backwards?
+    for(uint8_t drawX = this->x; drawX < (this->x + this->characterWidth); ++drawX)
     {
       uint8_t charY = 0;
-      for(uint8_t drawY = this->y; drawY < (this->y + this->CharacterHeight); ++drawY)
+      for(uint8_t drawY = this->y; drawY < (this->y + this->characterHeight); ++drawY)
       {
         const uint8_t colour = (charBuffer[charY] >> charX) & 0x1;
 
