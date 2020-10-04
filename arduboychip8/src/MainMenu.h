131bcbf7332c7d4ad55d2029c539d47057ba2dcf
spacebruce <noel.abercrombie@hotmail.com>
2020-10-04 12:41:17 +0100

    Split menu modes into functions

--- arduboychip8/src/MainMenu.h
+++ arduboychip8/src/MainMenu.h
@@ -5,7 +5,7 @@
 
 enum class MainMenuMode : uint8_t
 {
-	GameList, Settings, Load, 
+	GameList, Settings, Credits, Load, 
 };
 
 class MainMenu
@@ -19,40 +19,10 @@ private:
 //Contents
 	MainMenuMode mode = MainMenuMode::GameList;
   MenuHandler menu;
-public:
-	bool loadGame()
-	{
-		return (this->mode == MainMenuMode::Load);
-	}
-
-  void update(ButtonSystem& buttons)
+  
+  void drawGameList(Screen64x32& screen, Printer& printer)
   {
-	//Selection
-    const uint8_t menuSize = menu.getSize();
-    uint8_t selected = menu.getSelected();
-    if(buttons.justPressed(UP_BUTTON))
-      selected = (selected > 0) ? selected - 1 : 0;
-    if(buttons.justPressed(DOWN_BUTTON))
-      selected = (selected < (menuSize - 1)) ? selected + 1 : menuSize - 1;
-    menu.setSelected(selected);
-	
-	//Do thing
-	if(buttons.justPressed(A_BUTTON))
-	{
-		this->mode = MainMenuMode::Load;
-	}
-	
-    //scroll menu
-    if(selected <= scrollMargin)
-      scroll = 0;
-    else if(selected > (menuSize - scrollMargin))
-      scroll = menuSize - lines;
-    else
-      scroll = selected - scrollMargin;
-  }
-  void render(Screen64x32& screen, Printer& printer)
-  {
-    const uint8_t selected = menu.getSelected();
+	const uint8_t selected = menu.getSelected();
     const uint8_t first = scroll;
     const uint8_t last = min(first + lines, menu.getSize());
 
@@ -73,5 +43,58 @@ public:
       printer.println(i);
     }
     printer.setInverted(false);
+  }
+  void drawCredits(Screen64x32& screen, Printer& printer)
+  {
+	  printer.setPosition(0,8);
+	  printer.print(F("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?"));
+  }
+public:
+	bool loadGame()
+	{
+		return (this->mode == MainMenuMode::Load);
+	}
+
+  void update(ButtonSystem& buttons)
+  {
+	//Selection
+    const uint8_t menuSize = menu.getSize();
+    uint8_t selected = menu.getSelected();
+    if(buttons.justPressed(UP_BUTTON))
+      selected = (selected > 0) ? selected - 1 : 0;
+    if(buttons.justPressed(DOWN_BUTTON))
+      selected = (selected < (menuSize - 1)) ? selected + 1 : menuSize - 1;
+    menu.setSelected(selected);
+	
+	if(buttons.justPressed(LEFT_BUTTON))
+		this->mode = MainMenuMode::Credits;
+	if(buttons.justPressed(RIGHT_BUTTON))
+		this->mode = MainMenuMode::GameList;
+	
+	//Do thing
+	if(buttons.justPressed(A_BUTTON))
+	{
+		this->mode = MainMenuMode::Load;
+	}
+	
+    //scroll menu
+    if(selected <= scrollMargin)
+      scroll = 0;
+    else if(selected > (menuSize - scrollMargin))
+      scroll = menuSize - lines;
+    else
+      scroll = selected - scrollMargin;
+  }
+  void render(Screen64x32& screen, Printer& printer)
+  {
+	switch(this->mode)
+	{
+		case MainMenuMode::GameList:
+			this->drawGameList(screen, printer);
+		break;
+		case MainMenuMode::Credits:
+			this->drawCredits(screen, printer);
+		break;
+	}
   }
 };
