fdc13440cb86991a017bbc9abffacc922fe9996e
spacebruce <noel.abercrombie@hotmail.com>
2020-10-04 05:07:18 +0100

    Move menu backing code to MenuHandler.h

---
+++ arduboychip8/src/MenuHandler.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class MenuEntry
+{
+private:
+public:
+};
+
+class MenuHandler
+{
+private:
+  uint8_t selected = 0;
+  uint8_t size = 4; //temp
+public:
+  void setSelected(const uint8_t selected)
+  {
+    this->selected = selected;
+  }
+  uint8_t getSelected() const
+  {
+    return this->selected;
+  }
+  uint8_t getSize() const
+  {
+    return this->size;
+  }
+};
