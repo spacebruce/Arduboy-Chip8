2a8ba965670920830dc7a434e50ed4eabba28ece
spacebruce <noel.abercrombie@hotmail.com>
2020-10-05 04:41:44 +0100

    Prepare main menu for multiple concurrent menus

--- arduboychip8/src/MainMenu.h
+++ arduboychip8/src/MainMenu.h
@@ -21,13 +21,43 @@ private:
   int8_t scroll = 0;   //first drawn line
 //Contents
 	MainMenuMode mode = MainMenuMode::GameList;
-  MenuHandler menu;
+  MenuHandler menuGamelist;
 
+  void scrollMenu(MenuHandler& menu)
+  {
+    const uint8_t selected = menu.getSelected();
+    const uint8_t menuSize = menu.getSize();
+    if(selected <= scrollMargin)
+      scroll = 0;
+    else if(selected > (menuSize - scrollMargin))
+      scroll = menuSize - lines;
+    else
+      scroll = selected - scrollMargin;
+  }
+
+  void tickGameList(ButtonSystem& buttons)
+  {
+    const uint8_t menuSize = menuGamelist.getSize();
+    uint8_t selected = menuGamelist.getSelected();
+    if(buttons.justPressed(UP_BUTTON))
+      selected = (selected > 0) ? selected - 1 : 0;
+    if(buttons.justPressed(DOWN_BUTTON))
+      selected = (selected < (menuSize - 1)) ? selected + 1 : menuSize - 1;
+    menuGamelist.setSelected(selected);
+
+    //Load selected game thing
+    if(buttons.justPressed(A_BUTTON))
+    {
+      this->mode = MainMenuMode::Load;
+    }
+
+    this->scrollMenu(menuGamelist);
+  }
   void drawGameList(Screen64x32& screen, Printer& printer)
   {
-	const uint8_t selected = menu.getSelected();
+    const uint8_t selected = menuGamelist.getSelected();
     const uint8_t first = scroll;
-    const uint8_t last = min(first + lines, menu.getSize());
+    const uint8_t last = min(first + lines, menuGamelist.getSize());
 
     //selection box. Kinda hacky, but this is the look I wanna go for
     const uint8_t selectedOffset = (this->yStart + (selected - first) * 6) - 1;
@@ -47,38 +77,15 @@ private:
     }
     printer.setInverted(false);
   }
-  void tickGameList(ButtonSystem& buttons)
+
+  void drawCredits(Screen64x32& screen, Printer& printer)
   {
-    const uint8_t menuSize = menu.getSize();
-    uint8_t selected = menu.getSelected();
-    if(buttons.justPressed(UP_BUTTON))
-      selected = (selected > 0) ? selected - 1 : 0;
-    if(buttons.justPressed(DOWN_BUTTON))
-      selected = (selected < (menuSize - 1)) ? selected + 1 : menuSize - 1;
-    menu.setSelected(selected);
-
-    //Load selected game thing
-    if(buttons.justPressed(A_BUTTON))
-    {
-      this->mode = MainMenuMode::Load;
-    }
-
-    //scroll menu
-    if(selected <= scrollMargin)
-      scroll = 0;
-    else if(selected > (menuSize - scrollMargin))
-      scroll = menuSize - lines;
-    else
-      scroll = selected - scrollMargin;
+	  printer.setPosition(0,yStart);
+	  printer.print(F("Credits\n @Spacebruce\n Pharap"));
   }
   void tickSettings(ButtonSystem& buttons)
   {
 
-  }
-  void drawCredits(Screen64x32& screen, Printer& printer)
-  {
-	  printer.setPosition(0,8);
-	  printer.print(F("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?"));
   }
   void drawSettings(Screen64x32& screen, Printer& printer)
   {
@@ -92,6 +99,18 @@ public:
 
   void update(ButtonSystem& buttons)
   {
+    ///Change menu
+    if(buttons.justPressed(LEFT_BUTTON))
+    {
+      this->mode = (this->mode == firstMenuPage) ? lastMenuPage :
+        static_cast<MainMenuMode>((static_cast<uint8_t>(this->mode) - 1));
+    }
+    if(buttons.justPressed(RIGHT_BUTTON))
+    {
+      this->mode = (this->mode == lastMenuPage) ? firstMenuPage :
+        static_cast<MainMenuMode>((static_cast<uint8_t>(this->mode) + 1));
+    }
+
     switch(this->mode)
     {
       case MainMenuMode::GameList:
@@ -100,18 +119,6 @@ public:
       case MainMenuMode::Settings:
         tickSettings(buttons);
       break;
-    }
-
-    if(buttons.justPressed(LEFT_BUTTON))
-    {
-      this->mode = (this->mode == firstMenuPage) ? lastMenuPage :
-        static_cast<MainMenuMode>((static_cast<uint8_t>(this->mode) - 1));
-    }
-
-    if(buttons.justPressed(RIGHT_BUTTON))
-    {
-      this->mode = (this->mode == lastMenuPage) ? firstMenuPage :
-        static_cast<MainMenuMode>((static_cast<uint8_t>(this->mode) + 1));
     }
   }
   void render(Screen64x32& screen, Printer& printer)
